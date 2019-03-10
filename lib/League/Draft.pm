a6f60457252270a9db8dafd54ea6f2567ec88234
dgunay <devingunay@gmail.com>
2019-03-10 16:13:42 -0700

    Add main menu loop, add help, add dispatch table

--- lib/League/Draft.pm
+++ lib/League/Draft.pm
@@ -12,11 +12,69 @@ our @EXPORT_OK = qw(
   run_app
 );
 
+# Controls which view the player goes to
+our %dispatch_table = (
+  'Q' => {
+    'name' => 'Quit',                 # What the user sees in the menu
+    'info' => ['Quits the program.'], # lines passed to the help dialogue
+    'sub'  => \&quit,                 # the coderef to run
+  },
+  'H' => {
+    'name' => 'Help',                 
+    'info' => ['Display the rules of each draft mode.'], 
+    'sub'  => \&help,                 
+  },
+  '1' => {
+    'name' => 'All-random',
+    'info' => [
+      "All players are assigned a random champion.",
+      "Players may reroll twice and trade champions with their teammates.",
+    ],
+    'sub'  => \&all_random,
+  },
+  '2' => {
+    'name' => 'Single Draft',
+    'info' => [
+      'All players are assigned three random champions',
+      'Players pick from their personal pool of three champions.'
+    ],
+    'sub'  => \&single_draft,
+  },
+  '3' => {
+    'name' => 'Random Draft',
+    'info' => ['All players take turns choosing from one big random pool of champions'],
+    'sub'  => \&random_draft,
+  },
+);
+
+sub quit {
+  exit(0);
+}
+
+sub help {
+  # Display help for each menu option
+  foreach my $key (sort keys %dispatch_table) {
+    my $obj = $dispatch_table{$key};
+
+    print $$obj{'name'} . "\n";
+    foreach my $line (@{$$obj{'info'}}) {
+      print " - $line\n";
+    }
+    print "\n"
+  }
+
+  # press any button to go back to menu
+  print "\nPress Enter to return to the main menu.";
+  get_user_input();
+}
+
+
+
 sub run_app {
   # TODO: register a die handler
-  my $status = 0;
-  until ($status eq 'quit') {
-    $status = do_one_main_loop();
+  # my $status = 0;
+  until ('forever' && 0) {
+    do_one_main_loop();
   }
 }
 
@@ -29,17 +87,59 @@ sub clear_screen {
 }
 
 sub do_one_main_loop {
-  # Print intro + options to user
-  print "LOL DRAFT\n";
-  print "\n";
+  clear_screen();
 
-  # Get user input
+  # TODO: make pretty
+  # Print intro + options to user
+  show_main_menu();
 
   # select from dispatch table which subprogram to run
+  print "\nSelect a draft mode, then press Enter: ";
+  my $mode = undef;
+  while (1) {
+    my $input = get_user_input();
+
+    # upcase the input
+    $input = uc($input);
+
+    eval { 
+      $mode = select_mode($input);
+      last;
+    };
+    print "Not a valid option. Try again: " if $@;
+  }
 
   # run the option
+  clear_screen();
+  $mode->();
+}
 
-  # return
+sub show_main_menu {
+  print "  ___________\n";
+  print " |           |\n";
+  print " | LOL DRAFT | \n";
+  print " |___________|\n";
+  print "\n";
+  foreach my $option (sort keys %dispatch_table) {
+    print "  ($option): " . $dispatch_table{$option}{'name'} . "\n";
+  }
+}
+
+sub select_mode {
+  my $key = shift;
+
+  if (exists $dispatch_table{$key}) {
+    return $dispatch_table{$key}{'sub'};
+  }
+
+  die "Key not found in dispatch table";
+}
+
+
+sub get_user_input {
+  my $input = <STDIN>;
+  chomp $input;
+  return $input;
 }
 
 sub is_valid_champ {
